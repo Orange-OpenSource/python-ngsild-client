8d8431ca53a035854f527c6ad693f1dd1188e677
fbattello <fabien.battello@orange.com>
2022-10-04 14:27:33 +0200

    Updated readthedoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -85,7 +85,8 @@ master_doc = "index"
 # a list of builtin themes.
 #
 
-html_theme = "sphinx_rtd_theme"
+# html_theme = "sphinx_rtd_theme"
+html_theme = "pydata_sphinx_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
